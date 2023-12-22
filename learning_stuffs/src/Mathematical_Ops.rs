c52bc8b76a767f64520661145fc5ccb72054e6e9
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2023-12-22 06:27:40 +0530

    just Started

---
+++ learning_stuffs/src/Mathematical_Ops.rs
@@ -0,0 +1,22 @@
+// Just mathematical operations
+fn main() {
+	let x = 3 + 4 ;
+	println!("Here is the addition of the two numbers {}",x);
+
+	let y = 400 - 57;
+	println!("Here is the difference between two numbers {}",y);
+
+	let z = 4 * 5 ;
+	println!("Here is the multiplication {} ",z );
+
+	let div = 56/7;
+	println!("here is the division {} ",div);
+
+	let trun = -78/32 ;
+	println!("here is the truncated division {} ", trun);
+
+	let rem = 67%3;
+	println!("Here is the remainder {} ",rem );
+
+
+}
