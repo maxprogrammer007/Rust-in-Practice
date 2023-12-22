72d0643206d66eeda86cf7c31ecae203dfbad697
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2023-12-22 06:53:33 +0530

    Shadowing

---
+++ learning_stuffs/src/Shadowing.rs
@@ -0,0 +1,14 @@
+// Let me show yu shadowing
+
+fn main() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
