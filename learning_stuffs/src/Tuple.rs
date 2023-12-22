ff0fd295c2a9f1c72c90684fc3faddd44f32b9ab
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2023-12-22 07:08:40 +0530

    Tuples

--- learning_stuffs/src/Tuple.rs
+++ learning_stuffs/src/Tuple.rs
@@ -1,1 +1,8 @@
-fn main() {}
+fn main() {
+	let tup: (i32,f64,u8) = ( 500, 76.88 , 2);
+	let (x,y,z) = tup;
+
+	println!("The value of x in tuple is : {}",x);
+	println!("The value of y in tuple is : {}",y);
+	println!("The value of z in tuple is : {}",z);
+}
