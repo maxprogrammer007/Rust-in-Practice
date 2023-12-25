d7185972427546143572dbef2ed52af52c6022f3
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2023-12-25 10:31:52 +0530

    Create p1.rs

---
+++ exercises from practice.rs/p1.rs
@@ -0,0 +1,10 @@
+/*
+// Fix the error below with least amount of modification to the code
+fn main() {
+    let x: i32; // Uninitialized but used, ERROR !
+    let y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5);
+    println!("Success!");
+}
+*/
