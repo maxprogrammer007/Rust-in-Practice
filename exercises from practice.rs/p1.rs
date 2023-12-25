e6d808ff4c90a1b6dbce6c9eb5bca10330ec91a1
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2023-12-25 10:32:48 +0530

    Update p1.rs

--- exercises from practice.rs/p1.rs
+++ exercises from practice.rs/p1.rs
@@ -8,3 +8,13 @@ fn main() {
     println!("Success!");
 }
 */
+// Solution
+
+// Fix the error below with least amount of modification to the code
+fn main() {
+    let x: i32 = 5; // Uninitialized but used, ERROR !
+    let y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5);
+    println!("Success!");
+}
