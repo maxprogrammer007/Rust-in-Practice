bfe100004137eb1dccdcd22638baf1fb91e58d11
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2023-12-25 14:37:58 +0530

    Create p5.rs

---
+++ exercises from practice.rs/p5.rs
@@ -0,0 +1,32 @@
+/*
+// Only modify `assert_eq!` to make the `println!` work(print `42` in terminal)
+fn main() {
+    let x: i32 = 5;
+    {
+        let x = 12;
+        assert_eq!(x, 5);
+    }
+
+    assert_eq!(x, 12);
+
+    let x = 42;
+    println!("{}", x); // Prints "42".
+}
+*/
+
+// solution
+
+
+// Only modify `assert_eq!` to make the `println!` work(print `42` in terminal)
+fn main() {
+    let x: i32 = 5;
+    {
+        let x = 12;
+        assert_eq!(x, 12);
+    }
+
+    assert_eq!(x, 5);
+
+    let x = 42;
+    println!("{}", x); // Prints "42".
+}
