37394350cac700ea838f4c543b20119be5a8abfc
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2023-12-25 10:35:04 +0530

    Create p2.rs

---
+++ exercises from practice.rs/p2.rs
@@ -0,0 +1,21 @@
+/*
+// Fill the blanks in the code to make it compile
+fn main() {
+    let __ __ = 1;
+    __ += 2; 
+    
+    assert_eq!(x, 3);
+    println!("Success!");
+}
+*/
+
+//solution
+
+// Fill the blanks in the code to make it compile
+fn main() {
+    let mut x = 1;
+    x += 2; 
+    
+    assert_eq!(x, 3);
+    println!("Success!");
+}
