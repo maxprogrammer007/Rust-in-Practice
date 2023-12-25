322489af75464391251571c1afa887eccc6a5072
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2023-12-25 10:36:33 +0530

    Create p3.rs

---
+++ exercises from practice.rs/p3.rs
@@ -0,0 +1,23 @@
+/*
+// Fix the error below with least amount of modification
+fn main() {
+    let x: i32 = 10;
+    {
+        let y: i32 = 5;
+        println!("The value of x is {} and value of y is {}", x, y);
+    }
+    println!("The value of x is {} and value of y is {}", x, y); 
+}
+*/
+
+
+// Fix the error below with least amount of modification
+fn main() {
+    let x: i32 = 10;
+
+    let y: i32 = 5;
+    {
+        println!("The value of x is {} and value of y is {}", x, y);
+    }
+    println!("The value of x is {} and value of y is {}", x, y); 
+}
