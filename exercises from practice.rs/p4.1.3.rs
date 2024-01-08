ddcc020a7b742d208382737ccb9c427d10180c18
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-01-08 21:54:35 +0530

    ok

---
+++ exercises from practice.rs/p4.1.3.rs
@@ -0,0 +1,24 @@
+/* 
+// Modify `assert_eq!` to make it work
+fn main() {
+    let x = 5;
+    assert_eq!("u32".to_string(), type_of(&x));
+
+    println!("Success!");
+}
+
+// Get the type of given variable, return a string representation of the type  , e.g "i8", "u8", "i32", "u32"
+fn type_of<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}*/
+fn main() {
+    let x: u32 = 5;
+    assert_eq!("u32".to_string(), type_of(&x));
+
+    println!("Success!");
+}
+
+// Get the type of given variable, return a string representation of the type  , e.g "i8", "u8", "i32", "u32"
+fn type_of<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}
