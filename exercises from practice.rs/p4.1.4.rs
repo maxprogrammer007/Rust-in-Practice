1a8666edf8232332d4e653f799d4d19b0e3fb816
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-01-25 20:48:22 +0530

    Create p4.1.4.rs

---
+++ exercises from practice.rs/p4.1.4.rs
@@ -0,0 +1,17 @@
+/* 
+// Fill the blanks to make it work
+fn main() {
+    assert_eq!(i8::MAX, __); 
+    assert_eq!(u8::MAX, __); 
+
+    println!("Success!");
+}*/
+
+
+// Fill the blanks to make it work
+fn main() {
+    assert_eq!(i8::MAX, 127); 
+    assert_eq!(u8::MAX, 255); 
+
+    println!("Success!");
+}
