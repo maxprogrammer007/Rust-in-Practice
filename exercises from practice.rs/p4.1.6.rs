99f2253f2f54fdb7fa6e7bf67e9ceeb043290cac
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-01-25 20:48:13 +0530

    Create p4.1.6.rs

---
+++ exercises from practice.rs/p4.1.6.rs
@@ -0,0 +1,17 @@
+/*
+// Modify `assert!` to make it work
+fn main() {
+    let v = 1_024 + 0xff + 0o77 + 0b1111_1111;
+    assert!(v == 1579);
+
+    println!("Success!");
+}*/
+
+
+// Modify `assert!` to make it work
+fn main() {
+    let v = 1_024 + 0xff + 0o77 + 0b1111_1111;
+    assert!(v == 1597);
+
+    println!("Success!");
+}
