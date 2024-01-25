7d41aff290248e67d369bcdcfd4fa6a716596bf5
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-01-25 20:47:55 +0530

    Create p4.1.10.rs

---
+++ exercises from practice.rs/p4.1.10.rs
@@ -0,0 +1,19 @@
+/*
+// Fill the blanks
+use std::ops::{Range, RangeInclusive};
+fn main() {
+    assert_eq!((1..__), Range{ start: 1, end: 5 });
+    assert_eq!((1..__), RangeInclusive::new(1, 5));
+
+    println!("Success!");
+} */
+
+
+// Fill the blanks
+use std::ops::{Range, RangeInclusive};
+fn main() {
+    assert_eq!((1..5), Range{ start: 1, end: 5 });
+    assert_eq!((1..=5), RangeInclusive::new(1, 5));
+
+    println!("Success!");
+}
