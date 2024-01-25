d094627f5601c4ce79bafef39ee49d24fbcf2a5c
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-01-25 20:48:03 +0530

    Create p4.1.9.rs

---
+++ exercises from practice.rs/p4.1.9.rs
@@ -0,0 +1,25 @@
+/* fn main() {
+    let mut sum = 0;
+    for i in -3..2 {
+        sum += i
+    }
+
+    assert!(sum == -3);
+
+    for c in 'a'..='z' {
+        println!("{}",c);
+    }
+} */
+
+fn main() {
+    let mut sum:i32= 0;
+    for i in -3..2 {
+        sum += i
+    }
+
+    assert!(sum == -5);
+
+    for c in 'a'..='z' {
+        println!("{}",c as u8);
+    }
+}
