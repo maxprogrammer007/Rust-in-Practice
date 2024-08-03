03a2ed01b586d41fdb64e90ac740c6eac93333b0
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-08-03 09:19:13 +0530

    Update main.rs

--- Learnings_from_live_streams/rust_variables/src/main.rs
+++ Learnings_from_live_streams/rust_variables/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     let x = 5;
     println!("The value of x is :{}", x);
+    // x = 6; // This will give an error because x is immutable
+    // shadowing - we can declare a new variable with the same name as the previous one
+    {
+        let x = 6;
+        println!("The value of x is :{}", x);
+    }
 
     let mut y = x;
     y = y + 4;
