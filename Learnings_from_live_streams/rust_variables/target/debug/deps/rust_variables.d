a5b970f013dbb08ef4883acbb5c88268b3c45832
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-08-03 08:56:55 +0530

    Create rust_variables.d

---
+++ Learnings_from_live_streams/rust_variables/target/debug/deps/rust_variables.d
@@ -0,0 +1,5 @@
+C:\Users\abhi\Documents\GitHub\Rust-in-Practice\Learnings_from_live_streams\rust_variables\target\debug\deps\rust_variables.exe: src/main.rs
+
+C:\Users\abhi\Documents\GitHub\Rust-in-Practice\Learnings_from_live_streams\rust_variables\target\debug\deps\rust_variables.d: src/main.rs
+
+src/main.rs:
