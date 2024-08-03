76bf817ec6dda0f159aff9ea4dd8beeb3ad87192
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-08-03 08:46:58 +0530

    Create hello_world.d

---
+++ Learnings_from_live_streams/hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\abhi\Documents\GitHub\Rust-in-Practice\Learnings_from_live_streams\hello_world\target\release\hello_world.exe: C:\Users\abhi\Documents\GitHub\Rust-in-Practice\Learnings_from_live_streams\hello_world\src\main.rs
