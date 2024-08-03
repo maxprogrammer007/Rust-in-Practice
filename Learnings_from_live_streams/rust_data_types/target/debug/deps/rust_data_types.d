ba44e7dfcd13af7b62dc64e188fb3971ef819c72
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-08-03 12:17:55 +0530

    Create rust_data_types.d

---
+++ Learnings_from_live_streams/rust_data_types/target/debug/deps/rust_data_types.d
@@ -0,0 +1,5 @@
+C:\Users\abhi\Documents\GitHub\Rust-in-Practice\Learnings_from_live_streams\rust_data_types\target\debug\deps\rust_data_types.exe: src/main.rs
+
+C:\Users\abhi\Documents\GitHub\Rust-in-Practice\Learnings_from_live_streams\rust_data_types\target\debug\deps\rust_data_types.d: src/main.rs
+
+src/main.rs:
