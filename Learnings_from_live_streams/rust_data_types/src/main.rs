0b74a1cfaed2e584c0c114463fa5475e421f3a53
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-08-04 11:52:06 +0530

    Update main.rs

--- Learnings_from_live_streams/rust_data_types/src/main.rs
+++ Learnings_from_live_streams/rust_data_types/src/main.rs
@@ -106,4 +106,10 @@ fn main() {
     let tup: (i32,f64,u8) = (500, 6.4 ,1);
     let (x, y, z) = tup;
     println!("x: {}, y: {}, z: {}", x, y, z);
+
+    // Accessing by index
+
+    for i in 0..3 {
+        println!("{}: {}", i, tup.0);
+    }
 }
