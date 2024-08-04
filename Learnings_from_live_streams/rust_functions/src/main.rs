674e01736648d4ae8aa89b1b4d6cefeeaafe3728
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-08-04 17:00:09 +0530

    Update main.rs

--- Learnings_from_live_streams/rust_functions/src/main.rs
+++ Learnings_from_live_streams/rust_functions/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+// Functions -  Functions are the building blocks of a Rust program. Functions are used to break the code into smaller and modular pieces. Functions are defined using the fn keyword. The syntax of a function is as follows:
+
+ fn main() {
     println!("Hello, world!");
+    another_function();
+}
+
+fn another_function() {
+    println!("Another function.");
 }
