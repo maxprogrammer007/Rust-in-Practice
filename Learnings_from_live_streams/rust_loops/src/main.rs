da9341c84dcd9b603115b8f51c784d749c67f715
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-08-25 16:10:48 +0530

    Update main.rs

--- Learnings_from_live_streams/rust_loops/src/main.rs
+++ Learnings_from_live_streams/rust_loops/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+ let mut counter = 0;
+    let result = loop{
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {}", result);
 }
