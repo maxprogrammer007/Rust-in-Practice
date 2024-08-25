8824ff4f348e214027de599c86493c668b7755dd
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-08-25 15:44:38 +0530

    Update main.rs

--- Learnings_from_live_streams/rust_control_flow/src/main.rs
+++ Learnings_from_live_streams/rust_control_flow/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+let number = 3 ;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 }
