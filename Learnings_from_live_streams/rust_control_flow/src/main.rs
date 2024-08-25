099f4a60c6132a4b04444b286f37d735b5407f6e
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-08-25 15:50:05 +0530

    Update main.rs

--- Learnings_from_live_streams/rust_control_flow/src/main.rs
+++ Learnings_from_live_streams/rust_control_flow/src/main.rs
@@ -6,4 +6,25 @@ let number = 3 ;
     } else {
         println!("condition was false");
     }
+
+    let condition = true;
+    let number = if condition {
+        5
+    } else {
+        6
+    };
+
+    // nested if else
+    let num = 12;
+    if num % 2 == 0 {
+        println!("{} is even", num);
+    } else {
+        println!("{} is od",num);
+
+        if num > 10 {
+            println!("{} is greater than 10", num);
+        } else {
+            println!("{} is less than 10", num);
+        }
+    }
 }
