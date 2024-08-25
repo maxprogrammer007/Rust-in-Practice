edc828fecc25c1adeca841874cbc9899c754461d
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2024-08-25 11:33:21 +0530

    Update main.rs

--- Learnings_from_live_streams/rust_functions/src/main.rs
+++ Learnings_from_live_streams/rust_functions/src/main.rs
@@ -12,7 +12,17 @@
     let k = sum_diff(23,98);
     println!("The sum and difference of the given numbmer is {} and {}",k.0,k.1);
      println!("The sum and difference of the given number is {:?}",k); //{:?} is used to print the tuple
+    let result1 = sum1(0, 10);
+    println!("The sum of 0 and 10 is: {}", result1);
  }
+
+// using early return
+fn sum1(num1 :i32,num2:i32) ->i32{
+    if num1 == 0 {
+        return 0;
+    }
+    num1+num2
+}
 
 fn sum(num1 :i32,num2:i32) ->i32{
     num1+num2
@@ -29,7 +39,7 @@ fn another_function1(number: i32) {
 // Statements are instructions that perform some action and do not return a value. Expressions evaluate to a resulting value. Let's see an example of both:
 
 fn statements_and_expressions() {
-    let y = 6; // statement
+    let _y = 6; // statement
     let x = { // block is an expression
         let y = 3;
         y + 1 // expression
